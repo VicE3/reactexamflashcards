1419f38aa39ce69464106d16a9161e9fd9276d7c
VicE3 <viclee953@gmail.com>
2017-09-11 13:51:01 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //this will check to see if you have spelled "gato" correctly or not. It checks 1 word out of 40 :(
-
+//But I think I was on the right track...
 import React, { Component } from 'react';
 import './App.css';
 
